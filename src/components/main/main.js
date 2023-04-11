e715db3e3868c76a878e9c472a9fc541e241ac9e
jaythomason-projects <jaythomason.projects@outlook.com>
2023-04-11 18:18:35 +1000

    chore: created components

---
+++ src/components/main/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from './form/form';
+import './main.css'
+
+export default function main() {
+  return (
+    <div className='main'>
+      <Form />
+    </div>
+  );
+}
