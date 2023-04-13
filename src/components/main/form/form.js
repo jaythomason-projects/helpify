22b4ae27020f20fa3c2325c2adfc19a4752c3a65
jaythomason-projects <jaythomason.projects@outlook.com>
2023-04-13 18:09:50 +1000

    refactor: selected class on button
    also removed form tag, caused issues

--- src/components/main/form/form.js
+++ src/components/main/form/form.js
@@ -23,38 +23,46 @@ function Form() {
 
   return (
     <div className='form-wrapper'>
-      <form>
-        <section className='instructions-section'>
-          <p>Choose the tone of your response:</p>
-          <div className='instruction-tabs'>
-            <button onClick={() => handleTabClick("Understanding")}>Understanding</button>
-            <button onClick={() => handleTabClick("Instructional")}>Instructional</button>
-          </div>
-          <p>{instruction}</p>
-        </section>
+      <section className='instructions-section'>
+        <p>Choose the tone of your response:</p>
+        <div className='instruction-tabs'>
+          <button 
+            className={selectedTab === "Understanding" ? "selected" : ""}
+            onClick={() => handleTabClick("Understanding")}
+          >
+            Understanding
+          </button>
+          <button 
+            className={selectedTab === "Instructional" ? "selected" : ""}
+            onClick={() => handleTabClick("Instructional")}
+          >
+            Instructional
+          </button>
+        </div>
+        <p>{instruction}</p>
+      </section>
 
-        <section className='message-section'>
-          <label htmlFor="original-message" className="message-label">Enter your message:</label>
-          <textarea
-            id='original-message'
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder='Enter your message'
-            rows={10}
-          />
+      <section className='message-section'>
+        <label htmlFor="original-message" className="message-label">Enter your message:</label>
+        <textarea
+          id='original-message'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder='Enter your message'
+          rows={10}
+        />
 
-          <button className='revise-message-button' onClick={() => reviseMessage(message, instruction, setRevisedMessage)}>Revise Message</button>
+        <button className='revise-message-button' onClick={() => reviseMessage(message, instruction, setRevisedMessage)}>Revise Message</button>
 
-          <label htmlFor="revised-message" className="message-label">Revised message:</label>
-          <textarea
-            id='revised-message'
-            value={revisedMessage}
-            placeholder='Revised message will appear here'
-            rows={10}
-            readOnly
-          />
-        </section>
-      </form>
+        <label htmlFor="revised-message" className="message-label">Revised message:</label>
+        <textarea
+          id='revised-message'
+          value={revisedMessage}
+          placeholder='Revised message will appear here'
+          rows={10}
+          readOnly
+        />
+      </section>
     </div>
   );
 }
