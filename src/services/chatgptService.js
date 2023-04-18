54fbdb795983a7f5f3aef117b74b5286eeddae6f
jaythomason-projects <jaythomason.projects@outlook.com>
2023-04-18 20:35:45 +1000

    refactor: add greeting/sign-off lowered temperature

--- src/services/chatgptService.js
+++ src/services/chatgptService.js
@@ -18,10 +18,10 @@ const chatgptService = {
       const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [
-          { role: "system", content: `You are a helpful assistant that revises the user's message based on the following instruction: ${instruction} Return your revised message only.`},
+          { role: "system", content: `You are a helpful assistant that revises the user's message based on the following instruction: ${instruction} Return your revised message only, with the greeting 'Hello' and the sign-off 'Regards'`},
           { role: "user", content: message },
         ],
-        temperature: 0.7,
+        temperature: 0.5,
       });
 
       return response.data.choices[0].message.content;
