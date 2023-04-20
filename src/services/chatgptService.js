847d20d72c74f2e1575d14444047627816c151e2
jaythomason-projects <jaythomason.projects@outlook.com>
2023-04-20 20:14:18 +1000

    refactor: fixed greeting/send-off

--- src/services/chatgptService.js
+++ src/services/chatgptService.js
@@ -19,10 +19,10 @@ const chatgptService = {
         model: "gpt-3.5-turbo",
         messages: [
           { role: "system", content: `You are a helpful assistant that revises the user's message based on the following instruction: ${instruction} 
-          Return only the revised message, starting with 'Hello' and ending with 'Regards'`},
+          Return only the revised message, starting with the greeting 'Hello' and ending with the send off 'Regards'`},
           { role: "user", content: message },
         ],
-        temperature: 0.6,
+        temperature: 0.7,
       });
 
       return response.data.choices[0].message.content;
